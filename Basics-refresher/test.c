f77f96d33306de57ca1bf93827ee5e65315192c8
Neeraj Maggvi <neerajmaggavi09@gmail.com>
2026-08-21 22:18:39 +0530

    Added programs

---
+++ Basics-refresher/test.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main() 
+{
+    int size;
+
+    printf("Enter the size : ");
+    scanf("%d",&size);
+
+    int arr[size];
+
+    printf("Enter the array elements : \n");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    printf("The array elements : \n");
+    for (int i = 0; i < size; i++)
+    {
+        printf(" %d",arr[i]);
+    }
+    printf("\n");
+    
+    for (int i = 0; i < size / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[size - i - 1];
+        arr[size - i - 1] = temp;
+    }
+
+    printf("The array elements after reversing : \n");
+    for (int i = 0; i < size; i++)
+    {
+        printf(" %d",arr[i]);
+    }
+    printf("\n");
+    
+    
+    return 0;
+}
