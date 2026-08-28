eb1b0d51e99c19b8413f1599ec7b23623c223ad8
Neeraj Maggvi <neerajmaggavi09@gmail.com>
2026-08-28 16:47:59 +0530

    Added code

---
+++ strings/strcat.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+char *my_strcat(char dst[], const char scr[]);
+
+int main() 
+{
+    char dst[20] = "Hello";
+    char scr[20] = " World";
+
+    char *ptr = my_strcat(dst, scr);
+
+    printf("Scr ====> %s\n",scr);
+    printf("Dst =====> %s\n",ptr);
+
+    return 0;
+}
+
+char *my_strcat(char dst[], const char scr[])
+{
+    int i = 0;
+    int j = 0;
+
+    while (dst[i] != '\0')
+    {
+        i++;
+    }
+    while(scr[j] != '\0')
+    {
+        dst[i] = scr[j];
+        j++;
+        i++;
+    }
+    dst[i] = '\0';
+
+    return dst;
+    
+}
