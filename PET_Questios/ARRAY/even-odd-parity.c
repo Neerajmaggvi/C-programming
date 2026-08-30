edf6cd6bcb620634016d2d45e669a0fd163d9e8e
Neeraj Maggvi <neerajmaggavi09@gmail.com>
2026-08-30 19:03:07 +0530

    Added program

---
+++ PET_Questios/ARRAY/even-odd-parity.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main() 
+{
+    int size;
+
+    printf("Enter the size : ");
+    scanf("%d",&size);
+
+    int arr[size];
+
+    printf("Enter the array elements : ");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    printf("Array elements ====> ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+        else
+        {
+            arr[i] = 1;
+        }
+        
+    }
+
+    printf("Array elements ====> ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    
+}
