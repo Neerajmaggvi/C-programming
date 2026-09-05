1ba91c5113e42a88d268ed2a512e68c2bb45507b
Neeraj Maggvi <neerajmaggavi09@gmail.com>
2026-09-05 10:23:20 +0530

    completed

--- strings/find_the_longest_palindrome_in_string.c
+++ strings/find_the_longest_palindrome_in_string.c
@@ -8,20 +8,72 @@ int main()
     printf("Enter the string : ");
     scanf("%[^\n]",str);
 
-    int z = 0;
+    int i = 0;
+    int count;
+    int ispalindrome;
+    int longestCount = 0;
+    int longestStart = 0;
 
-    while (str[z] != '\0')
+    while (str[i] != '\0')
     {
-        int i = 0;
-        int j = 0;
-
-        while (str[i] != ' ')
+        while (str[i] == ' ')
         {
             i++;
         }
+
+        if(str[i] == '\0')
+        {
+            break;
+        }
+
+        count = 0;
+        int startIndex = i;
+
+        while (str[i] != ' ' && str[i] != '\0')
+        {
+            i++;
+            count++;
+        }
+
+        int endIndex = i - 1;
+
+        int leftIndex = startIndex;
+        int rightIndex = endIndex;
+        ispalindrome = 1;
+
+        while (leftIndex < rightIndex)
+        {
+            if (str[leftIndex] != str[rightIndex])
+            {
+                ispalindrome = 0;
+                break;
+            }
+            leftIndex++;
+            rightIndex--;
+            
+        }
+
+        if (ispalindrome)
+        {
+            if (longestCount < count)
+            {
+                longestCount = count;
+                longestStart = startIndex;
+            }
+            
+        }
         
-        z++;
     }
+
+    printf("Longest palindrome : ");
+    for (int i = longestStart; i < longestStart + longestCount; i++)
+    {
+        printf("%c",str[i]);
+    }
+    printf("\n");
+
+    printf("Longest palindrome count : %d\n", longestCount);
+
     
     return 0;
 }
