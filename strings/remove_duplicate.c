e9dea251c87802cc6c30e90362024576cc95a048
Neeraj Maggvi <neerajmaggavi09@gmail.com>
2026-09-07 00:16:01 +0530

    Added

--- strings/remove_duplicate.c
+++ strings/remove_duplicate.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
 int main() 
 {
+    char str[20];
+
+    printf("Enter the string : ");
+    scanf("%s",str);
+
+    int len = 0;
+
+    while (str[len] != '\0')
+    {
+        len++;
+    }
+
+    for(int i = 1; i < len; )
+    {
+        int isduplicate = 0;
+
+        for(int j = i - 1; j >= 0; j--)
+        {
+            if(str[i] == str[j])
+            {
+                isduplicate = 1;
+                break;
+            }
+        }
+
+        if(isduplicate)
+        {
+            for(int j = i; j < len; j++)
+            {
+                str[j] = str[j + 1];
+            }
+            len--;
+        }
+        else
+        {
+            i++;
+        }
+    }
     
+    printf("After removing duplicate ======>%s\n",str);
+
     return 0;
 }
