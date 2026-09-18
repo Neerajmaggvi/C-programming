47f691db9fcba611f2c1e2aece6f8dcc4f2400ab
Neeraj Maggvi <neerajmaggavi09@gmail.com>
2026-09-18 21:42:53 +0530

    Added

---
+++ unformatted_functions/use_putchar.c
@@ -0,0 +1,22 @@
+/*QUESTION 3: Print Using putchar()
+---------------------------------
+Write a C program to print:
+
+HELLO
+
+using only putchar() for printing the characters.
+
+Do not use printf() to print HELLO.*/
+
+
+#include <stdio.h>
+int main() 
+{
+    putchar('H');
+    putchar('E');
+    putchar('L');
+    putchar('L');
+    putchar('O');
+    
+    return 0;
+}
